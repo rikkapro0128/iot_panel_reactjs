4a56fc01357284c42c7387c21bff8db5e9c9794b
miru <miru@pop-os.localdomain>
2022-08-07 21:27:17 +0700

    style sensor

--- src/components/node/index.js
+++ src/components/node/index.js
@@ -7,8 +7,12 @@ function Node(props) {
   }, [props["node-payload"].path]);
 
   return (
-    <div className="">
-      <SensorDefault size="180" value={24} unit={'%'} title={'Nhiệt độ'}/>
+    <div className="flex">
+      <SensorDefault reverseColor={true} ladder={100} size={ 150 } value={56} unit={'°C'} title={'Nhiệt độ'}/>
+      <SensorDefault size={ 150 } value={69} unit={'%'} title={'Độ ẩm'}/>
+      <SensorDefault size={ 150 } value={355} ladder={1000} unit={'ppm'} title={'Oxi'}/>
+      <SensorDefault reverseColor={true} size={ 150 } value={35} unit={'dB'} title={'Âm thanh'}/>
+      <SensorDefault reverseColor={true} size={ 150 } value={5} unit={'LPG'} title={'Khí ga'}/>
     </div>
   );
 }
--- src/components/sensor/index.js
+++ src/components/sensor/index.js
@@ -1,20 +1,41 @@
 import ProgressBar from 'react-customizable-progressbar';
+import { useEffect, useState } from 'react';
+
+const colorStart = {
+  h: 124,
+  s: 100,
+  l: 65
+};
+
+const getPercent = (value, ladder) => {
+  return Math.floor((value / ladder) * 100);
+}
 
 export function SensorDefault(props) {
+  const [color, setColor] = useState(colorStart);
+  const [percentProgress, setPercentProgress] = useState(0);
+
+  useEffect(() => {
+    let percentParse = getPercent(props.value, props.ladder || 100);
+    setPercentProgress(percentParse);
+    percentParse = props.reverseColor ? (props.ladder || 100) - percentParse : percentParse;
+    setColor({ ...color, h: Math.floor((percentParse * colorStart.h) / 100) });
+  }, [props.value])
+
   return (
-    <div className={`w-[${props.size}px] h-[${props.size}px]`}>
+    <div style={{ width: `${props.size}px`, height: `${props.size}px` }}>
       <ProgressBar
         { ...props }
+        progress={percentProgress}
         radius={100}
-        progress={80}
         strokeWidth={18}
-        strokeColor="#a0d468"
+        strokeColor={`hsl(${color.h}, ${color.s}%, ${color.l}%)`}
         strokeLinecap="round"
         trackStrokeWidth={18}
         counterClockwise
       >
         <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]">
-          <div className="text-3xl text-center">{props.value}{props.unit}</div>
+          <div className="lg:text-3xl md:text-base md:font-bold	 text-center">{props.value}{props.unit}</div>
           <div className="text-md text-center">{props.title}</div>
         </div>
       </ProgressBar>
